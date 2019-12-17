578e1a4a794d4bef893663e876b500602bde0f35
jcorea <jacorea410@gmail.com>
2019-12-16 21:47:23 -0800

    add store mock data for recipes.

---
+++ src/store/recipes.js
@@ -0,0 +1,14 @@
+const recipes = [
+    {
+        id: 1,
+        name: 'Mac & Cheese',
+        instructions: 'Cook the noodles, then add the water'
+    },
+    {
+        id:2,
+        name: 'Tofu Burger',
+        instructions: 'Thaw the frozen Tofu Burger, and grill for 10 minutes.'
+    }
+]
+
+export default recipes;
